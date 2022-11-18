622ea1c750f5ce78b40203c8e0c279f49ebc7e97
Marius <mawiit15@hs-esslingen.de>
2022-11-18 21:08:04 +0100

    feat: multipe connection tcp client implementation

--- tcp-server/tcp-server.py
+++ tcp-server/tcp-server.py
@@ -1,9 +1,9 @@
 import socket
 import json
 from collections import deque
-import argparse
+import threading
 from datetime import *
-
+import test
 
 class Dataconverter:
         def __init__(self) -> None:
@@ -16,7 +16,8 @@ class Dataconverter:
             self.conversion(timestamp)
         def conversion(self,timestamp):
             json_object = json.loads(self.que.popleft())
-            data = {"data": {"measurement": "machinedata","tags": {"serialnumber": str},"values": [{"ts": str,"uptime": int,"temp": float,"cycle": int}]}}
+            data = {"data": {"measurement": str,"tags": {"serialnumber": str},"values": [{"ts": str,"uptime": int,"temp": float,"cycle": int}]}}
+            data["data"]["measurement"] = json_object['data'][0]
             data['data']['tags']['serialnumber'] = json_object['data'][0]
             data['data']['values'][0]['temp'] = json_object['data'][6]
             data['data']['values'][0]['uptime'] = json_object['data'][7]
@@ -24,8 +25,9 @@ class Dataconverter:
             data['data']['values'][0]['ts'] = str(timestamp)
             conv_json_object = data
             self.conv_list.append(conv_json_object)
-            self.last_state("Last_converted_JSON.json",conv_json_object)
-            #print("Converted JSON Object :"+ self.conv_list[len(self.conv_list)-1]+"\n")
+            self.last_state(f"Last_converted_JSON {threading.get_ident()}.json",conv_json_object)
+            #print(threading.active_count())
+            print("Converted JSON Object :"+ str(self.conv_list[len(self.conv_list)-1])+"\n")
         
         def last_state(self,filename,data):
             try:
@@ -44,19 +46,50 @@ class Tcpsocket:
     def __init__(self,ip='127.0.0.1',port=7002) -> None:
         self.HOST = ip
         self.PORT = port
-    def listen(self,Dc: Dataconverter)->None:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:    #Creates socket-object with alias s
-            s.bind((self.HOST, self.PORT))                              #Binds the socket-object to the Ip and Port defined above
-            s.listen()                                                  #The socket-object will listen to the Port and Ip-Adress
-            conn, addr = s.accept()                                     #conn is the socket-object that got send by the client
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((self.HOST, self.PORT))
+        self.connect = True
+    
+    def handle_client(self,conn:socket.socket,Dc:Dataconverter): 
+        while self.connect:
+            data = conn.recv(1024)
+            if not data:
+                self.connect = False  
+            Dc.store_que(data,self.timestamp())
+            conn.send(data)
+        conn.close()
+        print(threading.active_count())
+    
+    def listen(self,Dc: Dataconverter)->None:                         
+        self.server.listen()
+        while True:
+            conn, addr = self.server.accept()
+            print(f"Connected by {addr}")
+            thread = threading.Thread(target=self.handle_client,args=(conn,Dc))
+            thread.start()
+            if(threading.active_count()<=1):
+                print("Ich hasse threads")
+                break
+            
+
+        
+
+        '''try:
+            self.server.listen()
+            conn, addr = self.server.accept()                                    
             with conn:                                                  
                 print(f"Connected by {addr}")                           
                 while True:
-                    data = conn.recv(1024)                              #data is the JSON object that got send 1024 Bytes max
+                    data = conn.recv(1024)                              
                     Dc.store_que(data,self.timestamp())
                     if not data:
                         break
-                    conn.sendall(data)                      
+                    conn.sendall(data)
+        except Exception as e:
+            print(f"Machinesim disconnected {addr}")'''
+
+    
+    
     def timestamp(self):
         dt = datetime.now()
         ts = str(dt.isoformat('T'))
@@ -68,3 +101,4 @@ if __name__ == '__main__':
     Sock = Tcpsocket()
     Dc = Dataconverter()
     Sock.listen(Dc)
+
