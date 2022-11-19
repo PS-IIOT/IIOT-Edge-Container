0ee9cabb755ed333f03a833d3e482463dd507f11
Friedrich Lohrmann <frloit01@hs-esslingen.de>
2022-11-19 22:27:43 +0100

    feat: enable use of .env file for local testing

--- backend/rpc_connection.py
+++ backend/rpc_connection.py
@@ -1,11 +1,17 @@
-
+import os
+from dotenv import load_dotenv
+from pathlib import Path
 
 class Rpcconnection:
-    def __init__(self,ip,port=80):
-        self.HOST = ip
-        self.PORT = port
+    def __init__(self,url='192.168.0.254/rpc',port=80):
+        dotenv_path = Path('backend\pers_data.env')
+        load_dotenv(dotenv_path=dotenv_path)
+        self.HOST = os.getenv('ip')
+        self.PORT = os.getenv('port')
+        self.user = os.getenv('user')
+        self.password = os.getenv('password')
 
 
     def send_data(self, converted_data):
-        print("send_data augerufen", converted_data)
+        print('rpc-ip: ', self.HOST)
 
