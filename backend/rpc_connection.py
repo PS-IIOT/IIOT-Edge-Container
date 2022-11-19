f235214f73f003ed63c2112877ea69cb3d2d28dd
Friedrich Lohrmann <frloit01@hs-esslingen.de>
2022-11-19 23:51:13 +0100

    feat:  implement rpc to create a session

--- backend/rpc_connection.py
+++ backend/rpc_connection.py
@@ -1,17 +1,34 @@
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+import requests
 
 class Rpcconnection:
-    def __init__(self,url='192.168.0.254/rpc',port=80):
+    def __init__(self):
         dotenv_path = Path('backend\pers_data.env')
         load_dotenv(dotenv_path=dotenv_path)
-        self.HOST = os.getenv('ip')
+        self.HOST = os.getenv('url')
         self.PORT = os.getenv('port')
         self.user = os.getenv('user')
         self.password = os.getenv('password')
+        i = 1
+
+    def session_create(self):
+        json_login = {"id": str,"jsonrpc": str,"method": str,"params":[str, str, str, {"user": str,"password": str}]}
+        json_login["id"] = i
+        i+=1
+        json_login["jsonrpc"] = "2.0"
+        json_login["method"] = "call"
+        json_login["params"][0] = "00000000000000000000000000000000"
+        json_login["params"][1] = "session"
+        json_login["params"][2] = "create"
+        json_login["params"][3]["user"] = self.user
+        json_login["params"][3]["password"] = self.password
+        login = requests.post(self.HOST, json=json_login)
+        json_response = login.json()
+        sid = json_response["result"][1]["sid"]
+        return sid
 
 
     def send_data(self, converted_data):
-        print('rpc-ip: ', self.HOST)
-
+        self.session_create()
