1f3c848d57c4b86767e4f11809ff95b8fd640e28
Micha <totsches@gmail.com>
2022-11-26 18:39:56 +0100

    feat(frontend): change design of cards and header icons

--- frontend/src/components/Cards.tsx
+++ frontend/src/components/Cards.tsx
@@ -6,19 +6,19 @@ interface CardProps {
 }
 export const Card = ({ machineID, temparture, cycles, upTime }: CardProps) => {
     return (
-        <div className="flex-col justify-between  w-48 h-48 bg-ColorCardBackground m-5 rounded-lg shadow-lg shadow-black">
-            <div className="flex justify-center items-center bg-ColorCardTopBottom w-full h-8 rounded-t-lg ">
+        <div className="flex-col justify-between  w-48 h-48 bg-ColorCardBackground m-5 rounded-lg shadow-md shadow-black">
+            <div className="flex justify-center items-center bg-ColorCardTopBottom w-full h-8 rounded-t-md ">
                 <p className="flex italic text-c text-black">
                     Maschine {machineID}
                 </p>
             </div>
-            <div className=" flex justify-center items-center w-full text-white text-8xl ml-2">
+            <div className=" flex justify-center items-center w-full text-white text-7xl ml-2">
                 {temparture}°
             </div>
-            <div className="flex justify-center text-white text-2xl mt-5">
+            <div className="flex justify-center text-white text-2xl mt-2">
                 {cycles} St
             </div>
-            <div className="flex justify-center  w-full rounded-b-lg h- mt-4">
+            <div className="flex justify-center w-full rounded-b-lg text-white mt-2">
                 {upTime}
             </div>
         </div>
