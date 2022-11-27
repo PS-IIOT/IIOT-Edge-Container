8f5982ea9ae9590bacd281067ba7068ba8a711e7
Leopold Stenger <lestit01@hs-esslingen.de>
2022-11-27 18:53:25 +0100

    feat: add test file for mongodb connection

---
+++ backend/db.py
@@ -0,0 +1,18 @@
+import pymongo
+
+myclient = pymongo.MongoClient('mongodb://root:rootpassword@localhost:27017')
+
+mydb = myclient["organisation"]
+
+# use collection named "developers"
+mycol = mydb["developers"]
+
+# a document
+developer = {"name": "Lini", "address": "Sweden"}
+
+# insert a document to the collection
+x = mycol.insert_one(developer)
+
+# list the databases
+for db in myclient.list_databases():
+    print(db)
