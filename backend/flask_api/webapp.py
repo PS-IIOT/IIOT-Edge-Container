b1f0ef673cae4079b0553e1f60e01d433b266fce
Friedrich Lohrmann <frloit01@hs-esslingen.de>
2022-11-28 01:20:16 +0100

    feat: implement endpoint for all machinedata

--- backend/flask_api/webapp.py
+++ backend/flask_api/webapp.py
@@ -1,8 +1,7 @@
-from flask import Flask, jsonify, make_response
-#from flask import current_app as app
+from flask import Flask
 from flask_pymongo import PyMongo
 from bson.json_util import dumps, loads
-
+import json
 
 app = Flask(__name__)
 def create_app():
@@ -14,10 +13,22 @@ app.config["MONGO_URI"] = "mongodb://root:rootpassword@localhost:27017/machineDa
 mongo = PyMongo(app)
 db = mongo.db
     
-@app.route('/api/v1/machine/<string:serialnumber>', methods=['GET'])
+@app.route('/api/v1/machines', methods=['GET'])
+def getAll():
+    collist = db.list_collection_names()
+    datalist = list()
+    for col in collist:
+        rec = db[col].find()
+        rec_list_element = list(rec)
+        json_data = dumps(rec_list_element[0])
+        datalist.append(json.loads(json_data))
+    return datalist
+
+
+@app.route('/api/v1/machines/<string:serialnumber>', methods=['GET'])
 def getMachine(serialnumber):
-    cursor = mongo.db[serialnumber].find({'measurement': serialnumber})
+    cursor = db[serialnumber].find({'measurement': serialnumber})
     list_cur = list(cursor)
-    json_data = dumps(list_cur)
+    json_data = dumps(list_cur[0])
     print(str(json_data))
     return json_data
