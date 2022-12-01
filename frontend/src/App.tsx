b10fa6eca0eae60b247f897364c2efeb5da5eb31
Micha <totsches@gmail.com>
2022-12-01 17:28:30 +0100

    fix: windows screen size

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,10 @@ import { MainComponent } from './components/MainComponent';
 import { FooterComponent } from './components/FooterComponent';
 
 export const App = () => {
+    const windowSize = window.screen.availHeight;
+    console.log(windowSize);
     return (
-        <div className="flex flex-col justify-between bg-ColorAppBackground">
+        <div className="flex flex-col h-screen justify-between bg-ColorAppBackground">
             <HeaderComponent />
             <MainComponent />
             <FooterComponent />
