f6913707bcfb8c9ca2243abbd7bff6e49665c403
Leopold Stenger <lestit01@hs-esslingen.de>
2022-12-08 23:50:14 +0100

    feat: implement router and restructure components

---
+++ frontend/src/views/Admin.tsx
@@ -0,0 +1,7 @@
+export const Admin = () => {
+    return (
+        <div className="flex-1 m-5 p-5 overflow-y-auto flex justify-around flex-wrap content-center gap-5">
+            <h1 className="text-5xl font-bold italic">Admin Page</h1>
+        </div>
+    );
+};
