574fa6175f64114f8062bb247ad438e303c2fe49
Marius <mawiit15@hs-esslingen.de>
2022-12-08 22:59:12 +0100

    feat: routes implementation complete

--- backend/flask_api/webapp.py
+++ backend/flask_api/webapp.py
@@ -17,40 +17,33 @@ db = mongo.db
 
 @app.route('/api/v1/machines', methods=['GET'])
 def getAllMachine():
-    dataList = []
-    cursor = db["Machinedata"].find({})
-    crusor_errorlog = db["Errorlog"].find({})
-    error = json_util.dumps(crusor_errorlog)
-    item = json_util.dumps(cursor)
-    dataList.append(item)
-    dataList.append(error)
-    print(f"{dataList}")
+    machineList = []
+    cursor = list(db["Machinedata"].find({}))
+    cursor_errorlog = list(db["Errorlog"].find({}))
+    for machine in cursor:
+        print(f"{machine}")
+        for error in cursor_errorlog:
+            print(f"{error}")
+            if machine["serialnumber"] == error["machine"]:    
+                machine["errorlog"] = error
+                machineList.append(machine)
+    item = json_util.dumps(machineList)
     return json.loads(item)
-
-
+    
 @app.route('/api/v1/machines/<string:serialnumber>', methods=['GET'])
 def getOneMachine(serialnumber):
-    dataList = []
-    cursor = db["Machinedata"].find({"serialnumber": serialnumber})
-    crusor_errorlog = db["Errorlog"].find({"machine":serialnumber})
-    item = json_util.dumps(cursor.next())
-    error =json_util.dumps(crusor_errorlog)
-    dataList.append(item)
-    dataList.append(error)
-    print(f"{dataList}")
+    cursor = list(db["Machinedata"].find({"serialnumber": serialnumber}))
+    crusor_errorlog = list(db["Errorlog"].find({"machine":serialnumber}))
+    cursor[0]["errorlog"] = crusor_errorlog[0]
+    print(cursor)
+    item = json_util.dumps(cursor[0])
     return json.loads(item)
 
-@app.route('/api/v1/machines/<string:serialnumber>', methods=['POST'])
-def addIp(serialnumber):
-    dataList = []
-    cursor = db["Machinedata"].find({"serialnumber": serialnumber})
-    crusor_errorlog = db["Errorlog"].find({"machine":serialnumber})
-    item = json_util.dumps(cursor.next())
+@app.route('/api/v1/machines/errors', methods=['GET'])
+def getAllErrors():
+    crusor_errorlog = list(db["Errorlog"].find({}))
     error =json_util.dumps(crusor_errorlog)
-    dataList.append(item)
-    dataList.append(error)
-    print(f"{dataList}")
-    return json.loads(item)
+    return json.loads(error)
 
 
 
