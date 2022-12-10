4ce81bbb75b9cf82474617a4351257a09e4b82fe
Leopold Stenger <lestit01@hs-esslingen.de>
2022-12-10 21:28:48 +0100

    feat (frontend): add sample code for allowlist

--- frontend/src/services/machine.service.ts
+++ frontend/src/services/machine.service.ts
@@ -1,3 +1,4 @@
+import { Allowlist } from '../models/allowlist.model';
 import { Machine } from '../models/machine.model';
 
 export async function getAllMachines(): Promise<Machine[]> {
@@ -5,5 +6,13 @@ export async function getAllMachines(): Promise<Machine[]> {
         `${import.meta.env.VITE_BACKEND_API_URL}/machines`
     );
     const json_data = (await response.json()) as Machine[];
+    return json_data;
+}
+
+export async function getAllowlist(): Promise<Allowlist> {
+    const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_API_URL}/machines/allowlist`
+    );
+    const json_data = (await response.json()) as Allowlist;
     return json_data;
 }
