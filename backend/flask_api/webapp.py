849c25ae576d44c97deaca398770f291d73c04a6
Leopold Stenger <lestit01@hs-esslingen.de>
2022-12-10 15:59:12 +0100

    fix: change errorlog to be type of list

--- backend/flask_api/webapp.py
+++ backend/flask_api/webapp.py
@@ -25,32 +25,38 @@ def getAllMachine():
         print(f"{machine}")
         for error in cursor_errorlog:
             print(f"{error}")
-            if machine["serialnumber"] == error["machine"]:    
-                machine["errorlog"] = error
+
+            if machine["serialnumber"] == error["machine"]:
+                machine["errorlog"] = []
+                machine["errorlog"].append(error)
                 machineList.append(machine)
     item = json_util.dumps(machineList)
     return json.loads(item)
-    
+
+
 @app.route('/api/v1/machines/<string:serialnumber>', methods=['GET'])
 def getOneMachine(serialnumber):
     cursor = list(db["Machinedata"].find({"serialnumber": serialnumber}))
-    crusor_errorlog = list(db["Errorlog"].find({"machine":serialnumber}))
+    crusor_errorlog = list(db["Errorlog"].find({"machine": serialnumber}))
     cursor[0]["errorlog"] = crusor_errorlog[0]
     print(cursor)
     item = json_util.dumps(cursor[0])
     return json.loads(item)
 
+
 @app.route('/api/v1/machines/errors', methods=['GET'])
 def getAllErrors():
     crusor_errorlog = list(db["Errorlog"].find({}))
-    error =json_util.dumps(crusor_errorlog)
+    error = json_util.dumps(crusor_errorlog)
     return json.loads(error)
+
 
 @app.route('/api/v1/machines', methods=['POST'])
 def insertIp():
     ip = request.json['ip']
-    db["Ip_whitelist"].update_one({"_id":1},{"$push":{"Ip_Adresses":ip}})
+    db["Ip_whitelist"].update_one({"_id": 1}, {"$push": {"Ip_Adresses": ip}})
     return "200"
+
 
 """ @app.route('/api/v1/machines', methods=['DELETE'])
 def deletetIp():
@@ -59,6 +65,7 @@ def deletetIp():
     return "Deleted" """
 """ Die deleteIp funktion funktioniert nicht so wie wir wollen da pull alle einträge entfernt die mit der 
 Ip  die wir als JSON bekommen übereinstimmen und da unsere Ip_Allowlist nur 127.0.0.1 hält entfernt es alles"""
+
 
 def create_app():
     with app.app_context():
