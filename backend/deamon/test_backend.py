65af2ffce7324c4052b1c783d3a3e9e3632641d0
Marius <mawiit15@hs-esslingen.de>
2023-01-05 12:55:47 +0100

    feat: more tests + setUp and tearDown method

--- backend/deamon/test_backend.py
+++ backend/deamon/test_backend.py
@@ -3,26 +3,29 @@ from .tcp_server import Tcpsocket
 import jsonschema
 
 class TestBackend(unittest.TestCase):
+    def setUp(self) -> None:
+        self.server = Tcpsocket()
+    def tearDown(self) -> None:
+        pass
 
     def test_json_validator(self):
-        server = Tcpsocket()
         with self.assertRaises(jsonschema.ValidationError):
-            server.json_validation({"version": "adstec-machine-sim-v0", "data": [
-                "test4", 4, False, False, False, False, 23.333333333333343, 71, 46]})       # int instead of boolean (4)
+            self.server.json_validation({"version": "adstec-machine-sim-v0", "data": [
+                "test4", 4, False, False, False, False, 23.333333333333343, 71, 46]})            # int instead of boolean (4)
         with self.assertRaises(jsonschema.ValidationError):
-            server.json_validation({"version": "adstec-machine-sim-v0", "data": [
-                1, False, False, False, False, False, 23.333333333333343, 71, 46]})         # int instead of string (1)
+            self.server.json_validation({"version": "adstec-machine-sim-v0", "data": [
+                1, False, False, False, False, False, 23.333333333333343, 71, 46]})              # int instead of string (1)
         with self.assertRaises(jsonschema.ValidationError):
-            server.json_validation("Hallo")                                                 # string instead of object
+            self.server.json_validation("Hallo")                                                 # string instead of object
         with self.assertRaises(jsonschema.ValidationError):
-            server.json_validation({"version": "adstec-machine-sim-v0", "daten": [          # daten instead of data
+            self.server.json_validation({"version": "adstec-machine-sim-v0", "daten": [          # daten instead of data
                 "test4", False, False, False, False, False, 23.333333333333343, 71, 46]})
         with self.assertRaises(jsonschema.ValidationError):
-            server.json_validation({"test": "adstec-machine-sim-v0", "data": [              # test instead of version
+            self.server.json_validation({"test": "adstec-machine-sim-v0", "data": [              # test instead of version
                 "test4", False, False, False, False, False, 23.333333333333343, 71, 46]})
         with self.assertRaises(jsonschema.ValidationError):
-            server.json_validation({"version": "adstec-machine-sim-v0", "data": [           # 1 more property
+            self.server.json_validation({"version": "adstec-machine-sim-v0", "data": [           # 1 more property
                 "test4", False, False, False, False, False, 23.333333333333343, 71, 46], "test": "test"})
         with self.assertRaises(jsonschema.ValidationError):
-            server.json_validation({"version": "adstec-machine-sim-v0", "data": [           # 1 less item in array
+            self.server.json_validation({"version": "adstec-machine-sim-v0", "data": [           # 1 less item in array
                 "test4", False, False, False, False, False, 23.333333333333343, 71]})
--- backend/flask_api/test_flaskapi.py
+++ backend/flask_api/test_flaskapi.py
@@ -1,5 +1,6 @@
 import unittest
 from .webapp import app
+import base64
 
 
 class TestFlaskApi(unittest.TestCase):
@@ -8,6 +9,28 @@ class TestFlaskApi(unittest.TestCase):
         app.config["TESTING"] = True
         app.config["DEBUG"] = False
         self.app = app.test_client()
+    
+    def tearDown(self) -> None:
+        pass
+
+    def open_with_auth(self, url, method, username, password,data=None):
+        if method == 'GET':
+            return self.app.open(url,
+                method=method,
+                headers={
+                    'Authorization': 'Basic ' + base64.b64encode(bytes(username + \
+                    ":" + password,"ascii")).decode("ascii")
+                },
+            )
+        else:
+            return self.app.open(url,
+                method=method,
+                headers={
+                    'Authorization': 'Basic ' + base64.b64encode(bytes(username + \
+                    ":" + password,"ascii")).decode("ascii")
+                },
+                json=data
+            )
 
     def test_getAllMachines(self):
         response = self.app.get('/api/v1/machines')
@@ -20,6 +43,21 @@ class TestFlaskApi(unittest.TestCase):
 
     def test_getAllErrors(self):
         response = self.app.get("/api/v1/machines/errors")
+        self.assertEqual(response.mimetype,"application/json")
+        self.assertEqual(response.status_code,200)
+
+    def test_insertIp(self):
+        response = self.open_with_auth("/api/v1/machines/allowlist","POST","admin","admin",{"ip":"127.0.0.1"})
+        self.assertEqual(response.mimetype,"application/json")
+        self.assertEqual(response.status_code,200)
+    
+    def test_deleteIp(self):
+        response = self.open_with_auth("/api/v1/machines/allowlist","DELETE","admin","admin",{"ip":"127.0.0.1"})
+        self.assertEqual(response.mimetype,"application/json")
+        self.assertEqual(response.status_code,200)
+
+    def test_getAllowlist(self):
+        response = self.open_with_auth("/api/v1/machines/allowlist","GET","admin","admin")
         self.assertEqual(response.mimetype,"application/json")
         self.assertEqual(response.status_code,200)
 
